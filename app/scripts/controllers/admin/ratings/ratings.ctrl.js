f431dcf06e109788a25b85ca68f48f61eead053e
Tesser Paolo <p.tesser921@gmail.com>
2015-05-08 20:10:38 +0200

    Title: [src] aggiunta dir ratings in user Desc: viene aggiunto il file di route, il controller, e il file di test. Non ancora implementati.
    Task: Reported in #id_task
    Option: [option][option]
    END

---
+++ app/scripts/controllers/admin/ratings/ratings.ctrl.js
@@ -0,0 +1,18 @@
+(function () {
+    'use strict';
+
+    /**
+     * Name: nome.js
+     * Author: Tesser Paolo
+     * Mail. p.tesser921@gmail.com
+     *
+     * Modify
+     * Version  Date        Author          Desc
+     * ==========================================================
+     * 0.0.1    aaaa-mm-dd  Tesser Paolo    description
+     * -----------------------------------------------------------
+     *
+     */
+
+
+})();
