fc8524bfc2fd02c31c28f1ec14a902f94be37e34
Tesser Paolo <p.tesser921@gmail.com>
2015-05-11 22:35:56 +0200

    Title: [src] test e2e di prova senza corpo, solo asserzione Desc: Task: Reported in #id_task Option: [option][option] END

--- test/e2e/login.e2e.js
+++ test/e2e/login.e2e.js
@@ -1,7 +1,7 @@
 describe('Login interaction', function() {
 
     it('should check if redirect works with a correct input ', function() {
-        browser.get('http://mashup-unipd.github.io');
+        // browser.get('http://mashup-unipd.github.io');
 
         // element( by.css('data-ng-submit='));
         /* TODO: refactor */
