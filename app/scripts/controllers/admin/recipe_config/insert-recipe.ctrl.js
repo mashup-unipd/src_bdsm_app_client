9a4ee264586296d84060cf1bc28b08bc948e8f4e
Tesser Paolo <p.tesser921@gmail.com>
2015-05-15 18:16:47 +0200

    Title: [src] inserito errore visivo se non sono state aggiunte almeno 2 metriche Desc: vengono inoltre aggiunti commenti al codice per renderlo più chiaro Task: Reported in #id_task Option: [option][option] END

--- app/scripts/controllers/admin/recipe_config/insert-recipe.ctrl.js
+++ app/scripts/controllers/admin/recipe_config/insert-recipe.ctrl.js
@@ -32,11 +32,12 @@
 
         vm.titleRecipe = '';
         vm.descRecipe = '';
-        vm.type = '';
+        vm.type = ''; // type of the category choose in the combobox
         vm.insertSuccess = false;
         vm.insertError = false;
+		vm.metricError = false;
 
-        // array of the category for a options field in the template view
+        // array of the categories for a combobox field in the template view
         vm.categories = [
             {
                 key: 'facebook',
@@ -108,10 +109,12 @@
 				var metric;
 				var tempMetricVal = [];
 
+				// pass every value in the object and save them in a temporary array
 				Object.keys(obj).forEach(function(key){
 					tempMetricVal.push(obj[key]);
 				});
 
+				// this it's possible because we know the structure of the object
 				metric = new MetricModel(tempMetricVal[0], tempMetricVal[1], tempMetricVal[2]);
 
 				return metric;
@@ -120,7 +123,7 @@
 
 			// if the Admin had insert almost two metrics, he can inserts a new Recipe
 			if (checkMetricsQuantity()){
-				// TODO: use AdminModel to retrieve necessary data
+				// TODO: use AdminModel or authService to retrieve necessary data, maybe it's possible do this thing with the token stored in local
 				var recipe = new RecipeInsertModel('12355458654');
 
 				recipe.setTitleRecipe(vm.titleRecipe);
@@ -135,6 +138,7 @@
 				vm.tempMetrics = [];
 				vm.metrics = [];
 				vm.insertSuccess = true;
+				vm.metricError = false;
 
 			} else {
 				console.log("Devi inserire almeno due metriche");
@@ -144,10 +148,11 @@
 
 
 		/**
-		 * TODO
-		 * @param cat
-		 * @param typeCat
-		 * @param val
+		 * This function create a local object with the value of a metric
+		 * and insert it in a temporary array that stores all locals metric object
+		 * @param cat : category of the metric
+		 * @param typeCat : type of the category
+		 * @param val : value of the category
 		 */
 		function addMetric(cat, typeCat, val){
 
@@ -162,7 +167,7 @@
 			// reset values of metric's fields after a success insert
 			vm.types = [];
 			vm.valueMetric = '';
-
+			vm.metricError = false;
 		}
 
 		/**
@@ -183,11 +188,11 @@
         }
 
 		/**
-		 * TODO
-		 * @returns {boolean}
+		 * This function checks if there are almost two metrics in the Recipe
+		 * @returns {boolean} true if there are almost two else false
 		 */
 		function checkMetricsQuantity(){
-			console.log("ok");
+			vm.metricError = true;
 			return (vm.tempMetrics.length >= 2);
 		}
 
