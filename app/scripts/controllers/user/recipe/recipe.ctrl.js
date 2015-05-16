881ffadec56b72578ba882b7a9a4321c901132e4
Tesser Paolo <p.tesser921@gmail.com>
2015-05-16 15:55:39 +0200

    Title: [src] aggiornata vista recipe con ctrl (stub) Desc: viene inserita una componente fittizia, per ora nel controller, ma da spostare nel recipeService, che restituisce un array di recipe.
    Task: Reported in #id_task
    Option: [option][option]
    END

--- app/scripts/controllers/user/recipe/recipe.ctrl.js
+++ app/scripts/controllers/user/recipe/recipe.ctrl.js
@@ -2,34 +2,73 @@
     'use strict';
 
     /**
-     * Name: nome.js
-     * Author: Tesser Paolo
-     * Mail. p.tesser921@gmail.com
+     * Name: app/scripts/controllers/user/recipe/recipe.ctrl.js
+     * Author: MashUp
+     * Mail. info@mashup-unipd.it
      *
      * Modify
      * Version  Date        Author          Desc
      * ==========================================================
-     * 0.0.1    2015-05-04  Tesser Paolo    codifica modulo
+     * 0.0.1    2015-05-04  Tesser Paolo    code module
      * -----------------------------------------------------------
      *
      */
 
     /**
      * @ngdoc function
-     * @name nameModule.controller:NameCtrl
+     * @name app.controller:NameCtrl
      * @description
-     * # NameCtrl
-     * Controller of the clientApp
+     * # RecipeCtrl
+     * Controller of the app
      */
 
+	// TODO: insert recipeService as param
     var RecipeCtrl = function () {
 
         var vm = this;
 
+		vm.listRecipes = getListOfRecipes();
+
+
+		////////////////
+
+		function getListOfRecipes(){
+
+			var list = [
+				{
+					idRecipe: '10',
+					titleRecipe: 'SportNike',
+					descRecipe: 'Questa recipe contiene i trend relativi alla Nike'
+				},
+				{
+					idRecipe: '11',
+					titleRecipe: 'StarWars',
+					descRecipe: 'Questa recipe contiene i trend relativi a StarWars'
+				},
+				{
+					idRecipe: '12',
+					titleRecipe: 'Boobies',
+					descRecipe: 'Questa recipe contiene i trend relativi alle migliori tette'
+				},
+				{
+					idRecipe: '13',
+					titleRecipe: 'Bear',
+					descRecipe: 'Questa recipe contiene i trend relativi alle migliori birre'
+				}
+			];
+
+
+
+			return list;
+
+		}
+
     };
 
-    RecipeCtrl.$inject = [];
+    RecipeCtrl.$inject = []; // TODO: inject recipeService as param
 
-    angular.module('app').controller('RecipeCtrl', RecipeCtrl);
+    angular
+		.module('app')
+		.controller('RecipeCtrl', RecipeCtrl);
 
 })();
