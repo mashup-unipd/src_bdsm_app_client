8f91cc175ded7b249c8ec330c6f3aec5aa94c011
Tesser Paolo <p.tesser921@gmail.com>
2015-05-19 16:09:04 +0200

    Title: [src] aggiunti servizi e funzioni per gestione access token Desc: Task: Reported in #34801354355632 Option: [complete][option] END

--- app/scripts/model/services/user/user.service.js
+++ app/scripts/model/services/user/user.service.js
@@ -12,6 +12,8 @@
 	 * ==========================================================
 	 * 0.0.1    2015-05-14  Tesser Paolo    code module
 	 * -----------------------------------------------------------
+	 * 0.0.2	2015-05-18	Tesser Paolo	insert function for access token
+	 * -----------------------------------------------------------
 	 *
 	 */
 
@@ -28,14 +30,14 @@
 
 		var factory = {
 			getFavourites: getFavourites,
-			addFavourite: addFavourite
+			addFavourite: addFavourite,
+			getAccessToken: getAccessToken,
+			deleteAccessToken: deleteAccessToken
 		};
 
 		return factory;
 
 		///////////////
-
-		// TODO: implement private and public functions
 
 		/**
 		 * TODO
@@ -45,6 +47,7 @@
 		function getFavourites( idUser ){
 
 			// TODO: it musts returns an array of object
+
 		}
 
 		/**
@@ -54,6 +57,25 @@
 		 */
 		function addFavourite( idUser ){
 
+		}
+
+		/**
+		 * This function retries an access token generates from the back-end application
+		 * TODO (test):
+		 * @param idUser : id of the user that we want obtained an access token
+		 * @return {String} : access token returned
+		 */
+		function getAccessToken( idUser ){
+			var token = '32325346267542524';
+			return token;
+		}
+
+		/**
+		 * This function deletes an access token from the back-end application
+		 * @param idUser : id of the user that we want deleted an access token
+		 */
+		function deleteAccessToken( idUser ){
+			// TODO: delete access token with an API calls
 		}
 	}
 
