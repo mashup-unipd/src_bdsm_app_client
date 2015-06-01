08779b509c597efb38d24d01a9672f0ecbe9d830
Tesser Paolo <p.tesser921@gmail.com>
2015-06-01 10:21:48 +0200

    Title: [src] refactor test e2e con PageObjects Desc: Task: Reported in #id_task Option: [option][option] END

--- test/e2e/login.e2e.js
+++ test/e2e/login.e2e.js
@@ -1,8 +1,36 @@
+/**
+ * Name: test/e2e/login.e2e.js
+ * Author: MashUp
+ * Mail: info@mashup-unipd.it
+ *
+ * Modify
+ * Version  Date        Author          Desc
+ * ==========================================================
+ * 0.0.1    2015-05-10  Tesser Paolo    code module
+ * -----------------------------------------------------------
+ * 0.0.2    2015-05-30  Tesser Paolo    insert login page like PageObject pattern
+ * -----------------------------------------------------------
+ *
+ */
+
+
+var AngularLoginPage = function(){
+
+	// TODO: variables that get element from page
+
+	this.get = function(){
+		browser.get('http://mashup-unipd.github.io');
+	}
+
+};
+
+
+
 describe('Login interaction', function() {
 
     it('should check if redirect works with a correct input ', function() {
-        browser.get('http://mashup-unipd.github.io');
-
+		var angularLoginPage = new AngularLoginPage();
+		angularLoginPage.get();
 
         /* TODO: refactor */
 
