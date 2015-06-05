7aaad3a46d0b1a273d4d2fda3a9fecb8a493d40d
Tesser Paolo <p.tesser921@gmail.com>
2015-06-05 14:32:05 +0200

    Title: [src] iniziati test per metrics ctrl Desc: Task: Reported in #id_task Option: [option][option] END

--- app/scripts/controllers/user/recipe/metrics.ctrl.spec.js
+++ app/scripts/controllers/user/recipe/metrics.ctrl.spec.js
@@ -9,6 +9,8 @@
  * ==========================================================
  * 0.0.1    2015-05-15  Tesser Paolo    code module
  * -----------------------------------------------------------
+ * 0.0.2		2015-06-03	Tesser Paolo		add test with real promise implement
+ * -----------------------------------------------------------
  *
  */
 
@@ -16,22 +18,67 @@
 describe('Controller: MetricsCtrl', function() {
 	'use strict';
 
-	var $controller;
+	var $controller = undefined;
+	var $rootScope = undefined;
+	var scope = undefined;
+	var recipeService = undefined;
+	var deferred = undefined;
+	var stateparams = {};
+	var title = undefined;
+
+	////////////////
 
 	beforeEach(function () {
+		angular.mock.module('app.recipe.services.module');
 		angular.mock.module('app');
 	});
 
 	beforeEach(function(){
-		angular.mock.inject(function (_$controller_) {
+		angular.mock.inject(function (_$controller_, _$rootScope_, _$q_, _recipeService_) {
 			$controller = _$controller_;
+			$rootScope = _$rootScope_;
+			scope = $rootScope.$new();
+			deferred = _$q_.defer();
+			recipeService = _recipeService_;
+
+		stateparams = {
+			title: 'TestTitle'
+		};
+
+
+
 		});
 	});
 
+	beforeEach(function(){
+		$controller('MetricsCtrl as sc', {
+			$scope: scope,
+			$stateParams: stateparams,
+			recipeService: recipeService
+		});
+	});
+
+	////////////////
 
 	it('should have a MetricsCtrl controller', function () {
 		expect($controller('MetricsCtrl')).toBeDefined();
 	});
+
+	it('should getMetricTypes is defined and calls recipeService', function(){
+		// defined
+		expect(scope.sc.getMetricTypes).toBeDefined();
+
+		// and calls recipeService
+		scope.sc.getMetricTypes();
+		expect(recipeService.getMetricTypes).toHaveBeenCalled();
+
+	});
+
+
+
+
+
+
 
 
 });
